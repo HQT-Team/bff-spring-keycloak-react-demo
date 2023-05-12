63a1260fcce058f847e571d74cf0051a51820b5b
khoahd7621 <hoangdangkhoa7621@gmail.com>
2023-05-12 18:11:51 +0700

    Setup backend product service

---
+++ product/src/main/java/com/hqt/product/controller/TestController.java
@@ -0,0 +1,28 @@
+package com.hqt.product.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/admin")
+    public ResponseEntity<String> getAdmin(Principal principal) {
+        JwtAuthenticationToken token = (JwtAuthenticationToken) principal;
+        String userName = (String) token.getTokenAttributes().get("name");
+        String userEmail = (String) token.getTokenAttributes().get("email");
+        return ResponseEntity.ok("Hello Admin \nUser Name : " + userName + "\nUser Email : " + userEmail);
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<String> getUser(Principal principal) {
+        JwtAuthenticationToken token = (JwtAuthenticationToken) principal;
+        String userName = (String) token.getTokenAttributes().get("name");
+        String userEmail = (String) token.getTokenAttributes().get("email");
+        return ResponseEntity.ok("Hello User \nUser Name : " + userName + "\nUser Email : " + userEmail);
+    }
+}
---
+++ product/src/test/java/com/hqt/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hqt.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
